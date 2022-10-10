9358b07a565488e3f1fe1089ed6d4716e6e5f464
fixss <fixssss@gmail.com>
2022-10-10 22:22:50 +0300

    Created for bin

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,29 @@
 import ru.netology.Basket;
-
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Scanner scanner = new Scanner(System.in);
         String[] products = {"Bread", "Cornflakes", "Milk", "Eggs", "Meat"};
         int[] prices = {50, 200, 100, 120, 500};
-        Basket store = new Basket (prices, products);
-        Basket.setArrayBasket(products.length);
-        String textFileBasket = "D:\\Repository\\Products/basket.txt";
-        File basketText = new File("basket.txt");
-        if ((basketText).exists()) {
+        Basket cart = new Basket(prices, products);
+        String binFileBasket = "basket.bin";
+        File basketBin = new File("D:\\Repository\\Products/basket.bin");
+        if (Files.exists(Path.of(String.valueOf(binFileBasket)))) {
             System.out.println("You have previously created basket");
-            store.loadFromTextFile(basketText);
+            cart = Basket.loadFromBinFile(basketBin);
+            cart.printCart();
         } else {
             try {
-                if (basketText.createNewFile()) {
+                if (basketBin.createNewFile()) {
                     System.out.println("New Basket crated");
                 }
             } catch (IOException e) {
-                System.out.println("Folder " + textFileBasket + " is missing.");
+                System.out.println("Folder " + binFileBasket + " is missing.");
                 throw new RuntimeException(e);
             }
         }
@@ -34,20 +35,19 @@ class Main {
         while (true) {
             int productNumber = 0;
             int productCount = 0;
-            System.out.println("Enter product number and amount or press `end` for finishing");
+            System.out.println("Enter product number and amount or press `end` for finishing" + "\n");
             String inputString = scanner.nextLine();
             if (inputString.equals("end")) {
-                store.addToCart(productNumber, productCount);
+                cart.printCart();
+                cart.saveBin(basketBin);
+                System.out.println("File: D:\\Repository\\Products/basket.bin saved");
                 break;
             }
             String[] inputProduct = inputString.split(" ");
             productNumber = Integer.parseInt(inputProduct[0]) - 1;
             productCount = Integer.parseInt(inputProduct[1]);
-            store.addToCart(productNumber, productCount);
-            store.saveTxt(new File(textFileBasket));
+            cart.addToCart(productNumber, productCount);
+            cart.printCart();
         }
-        store.printCart();
-
     }
-
 }
--- src/ru/netology/Basket.java
+++ src/ru/netology/Basket.java
@@ -2,19 +2,16 @@ package ru.netology;
 
 import java.io.*;
 
-public class Basket {
-    protected final int[] prices;
-    protected final String[] products;
-    private static int[] arrayBasket;
+public class Basket implements Serializable {
+    protected int[] prices;
+    protected String[] products;
+    public int[] arrayBasket;
     private int sumProducts = 0;
 
     public Basket(int[] prices, String[] products) {
         this.prices = prices;
         this.products = products;
-    }
-
-    public static void setArrayBasket(int productsLength) {
-        arrayBasket = new int[productsLength];
+        this.arrayBasket = new int[products.length];
     }
 
     public void addToCart(int productNum, int amount) {
@@ -23,51 +20,29 @@ public class Basket {
     }
 
     public void printCart() {
-        System.out.println("Your Basket:");
+        System.out.println("Your Basket contains:" + "\n");
         for (int i = 0; i < products.length; i++) {
             if (arrayBasket[i] > 0) {
                 int sumProduct = arrayBasket[i] * prices[i];
                 System.out.println(products[i] + " " + arrayBasket[i] + " item " + prices[i] + " rub/item. " + " Total: " + sumProduct );
             }
         }
-        System.out.println("Total: " + sumProducts + " rub");
+        System.out.println("\n" + "Total: " + sumProducts + " rub" + "\n");
     }
 
-    public void saveTxt(File textFile) throws IOException {
-
-        try (PrintWriter out = new PrintWriter(new FileWriter(textFile))) {
-
-            for (int e : arrayBasket)
-                out.print(e + " ");
-        } catch (IOException e) {
-            System.out.println("File: D:\\Repository\\Products/basket.txt is missing or wrong path");
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void loadFromTextFile(File textFile) throws IOException {
-        try (BufferedReader in = new BufferedReader(new FileReader(textFile))) {
-            String[] itemSplit = in.readLine().split(" ");
-            for (int i = 0; i < itemSplit.length; i++) {
-                arrayBasket[i] = Integer.parseInt(itemSplit[i]);
-            }
-        }
-    }
-
-    protected void saveBin(File file) throws IOException {
-        try (FileOutputStream fos = new FileOutputStream(file);
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(this);
-            System.out.println("Файл Basket.bin created.");
+    public void saveBin(File file) throws IOException {
+        try (ObjectOutputStream saveBinFile = new ObjectOutputStream(new FileOutputStream(file))) {
+            saveBinFile.writeObject(this);
+            System.out.println("File: D:\\Repository\\Products/basket.bin created");
         }
     }
 
     public static Basket loadFromBinFile(File file) throws IOException, ClassNotFoundException {
         try (ObjectInputStream loadBinFile = new ObjectInputStream(new FileInputStream(file))) {
-            Basket baskets = (Basket) loadBinFile.readObject();
+            Basket cart = (Basket) loadBinFile.readObject();
             loadBinFile.close();
-            System.out.println("Файл Basket.bin read by using Deserialization method");
-            return baskets;
+            System.out.println("File: D:\\Repository\\Products/basket.bin loaded");
+            return cart;
+            }
         }
     }
-}
