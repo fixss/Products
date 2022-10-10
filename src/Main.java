772213d7895251787d32e9c833fea915b20e609b
fixss <fixssss@gmail.com>
2022-10-10 21:13:37 +0300

    Products recreated

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,53 @@
-import ru.netology.Products;
+import ru.netology.Basket;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        Products[] products = new Products[3];
-        products[0] = new Products(75.99, "Milk");
-        products[1] = new Products(50.00, "Bread");
-        products[2] = new Products(152.35, "Cornflakes");
+        String[] products = {"Bread", "Cornflakes", "Milk", "Eggs", "Meat"};
+        int[] prices = {50, 200, 100, 120, 500};
+        Basket store = new Basket(prices, products);
+        Basket.setArrayBasket(products.length);
+        String textFileBasket = "D:\\Repository\\Products/basket.txt";
+        File basketText = new File("basket.txt");
+        if ((basketText).exists()) {
+            System.out.println("You have previously created basket");
+            store.loadFromTxtFile(basketText);
+        } else {
+            try {
+                if (basketText.createNewFile()) {
+                    System.out.println("New Basket crated");
+                }
+            } catch (IOException e) {
+                System.out.println("Folder " + textFileBasket + " is missing.");
+                throw new RuntimeException(e);
+            }
+        }
 
-        double sumProducts = 0;
-        int[] bucket = new int[3];
-        System.out.println("List of possible items to buy:");
+        System.out.println("Products available for purchase: ");
         for (int i = 0; i < products.length; i++) {
-            System.out.println(i + 1 + ". " + products[i].getName() + " " + products[i].getPrice() + " rub/item");
+            System.out.println((i + 1) + ". " + products[i] + " " + prices[i] + " rub/item");
         }
         while (true) {
-            System.out.println("Select item and quantity or enter `end` ");
-            String input = scanner.nextLine();
-            if (input.equals("end")) {
-                System.out.println("Your bucket: " + '\n');
-                for (int i = 0; i < bucket.length; i++) {
-                    if (bucket[i] != 0) {
-                        System.out.println(products[i].getName() + " " + bucket[i] + " items " + products[i].getPrice() + " rub/item " + bucket[i] * products[i].getPrice() + " rubles in total");
-                    }
-
-                }
-                System.out.println("Total " + sumProducts + " Rubles");
+            int productNumber = 0;
+            int productCount = 0;
+            System.out.println("Enter product number and amount or press `end` for finishing");
+            String inputString = scanner.nextLine();
+            if (inputString.equals("end")) {
+                store.addToCart(productNumber, productCount);
                 break;
-
             }
-            String[] choice = input.split(" ");
-            int chosenProduct = Integer.parseInt(choice[0]) - 1;
-            int amount = Integer.parseInt(choice[1]);
-            bucket[chosenProduct] += amount;
-            double currentPrice = products[chosenProduct].getPrice();
-            sumProducts += currentPrice * bucket[chosenProduct];
+            String[] inputProduct = inputString.split(" ");
+            productNumber = Integer.parseInt(inputProduct[0]) - 1;
+            productCount = Integer.parseInt(inputProduct[1]);
+            store.addToCart(productNumber, productCount);
+            store.saveTxt(new File(textFileBasket));
         }
+        store.printCart();
+
     }
 
 }
---
+++ src/ru/netology/Basket.java
@@ -0,0 +1,54 @@
+package ru.netology;
+
+import java.io.*;
+
+public class Basket {
+    protected int[] prices;
+    protected String[] products;
+    private static int[] arrayBasket;
+    private int sumProducts = 0;
+
+    public Basket(int[] prices, String[] products) {
+        this.prices = prices;
+        this.products = products;
+    }
+
+    public static void setArrayBasket(int productsLength) {
+        arrayBasket = new int[productsLength];
+    }
+
+    public void addToCart(int productNum, int amount) {
+        sumProducts += prices[productNum] * amount;
+        arrayBasket[productNum] += amount;
+    }
+
+    public void printCart() {
+        System.out.println("Your Basket:");
+        for (int i = 0; i < products.length; i++) {
+            if (arrayBasket[i] > 0) {
+                int sumProduct = arrayBasket[i] * prices[i];
+                System.out.println(products[i] + " " + arrayBasket[i] + " item " + prices[i] + " rub/item. " + " Total: " + sumProduct);
+            }
+        }
+        System.out.println("Total: " + sumProducts + " rub");
+    }
+
+    public void saveTxt(File textFile) throws IOException {
+        try (PrintWriter out = new PrintWriter(new FileWriter(textFile))) {
+            for (int e : arrayBasket)
+                out.print(e + " ");
+        } catch (IOException e) {
+            System.out.println("File: D:\\Repository\\Products/basket.txt is missing or wrong path");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void loadFromTxtFile(File textFile) throws IOException {
+        try (BufferedReader in = new BufferedReader(new FileReader(textFile))) {
+            String[] itemSplit = in.readLine().split(" ");
+            for (int i = 0; i < itemSplit.length; i++) {
+                arrayBasket[i] = Integer.parseInt(itemSplit[i]);
+            }
+        }
+    }
+}
